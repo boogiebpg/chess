71519f05899ac103af745bc036ed06736314fb84
Pavel <pavel@kobemail.com>
2012-06-13 21:39:18 +0300

    edit .gitignore, remove photos from public folder, fix excel loading

--- app/models/excel.rb
+++ app/models/excel.rb
@@ -1,9 +1,15 @@
 class Excel
 
   def check_format_and_save row
-    if row[2].to_f>0
+    # check price
+    if row[5].to_f>0
       p = Price.new
-      p.update_attributes(:name => row[0], :description => row[1], :value => row[2])
+
+      # check category
+      cat = Category.where(id: row[0]).first
+      cat_id = (cat.nil? ? 0 : row[0])
+
+      p.update_attributes(:category_id => cat_id, :name_en => row[1], :name_ru => row[2], :description_en => row[3], :description_ru => row[4], :value => row[5])
       p.save!
       return 1
     else
